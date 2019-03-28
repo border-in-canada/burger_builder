4bea9597e64e9e045d338d13e9f0e4a768b09ebd
border-in-canada <mathesd84@gmail.com>
2019-03-28 15:39:36 -0700

    Added modal, backdrop, checkout buttons, and nav

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,14 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelledHandler = () => {
+        this.setState({purchasing: false});
+    }
+
+    purchaseContinuedHandler = () => {
+        alert('You Continue!');
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,8 +93,12 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                    ingredients={this.state.ingredients} 
+                    price={this.state.totalPrice}
+                    purchaseCancelled={this.purchaseCancelledHandler}
+                    purchaseContinued={this.purchaseContinuedHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
